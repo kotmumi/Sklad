97ca3b7b7f5e93960c93dc9d0269619db59a5971
Kotylo <kirillkotylo@gmail.com>
2025-07-29 19:08:12 +0300

    Initial Commit

---
+++ Sklad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sklad
+//
+//  Created by Кирилл Котыло on 29.07.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
