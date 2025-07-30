8723f644a4b9ef9871715c8793d6c2a0437066bd
Kotylo <kirillkotylo@gmail.com>
2025-07-30 08:03:25 +0300

    coordinator add

---
+++ Sklad/Application/Coordinators/MainCoordinator.swift
@@ -0,0 +1,41 @@
+//
+//  MainCoordinator.swift
+//  Sklad
+//
+//  Created by Кирилл Котыло on 29.07.25.
+//
+
+import UIKit
+
+final class MainCoordinator: Coordinator {
+    
+    var navigationController: UINavigationController
+    var childCoordinators: [Coordinator] = []
+    private var tabBarController: UITabBarController = UITabBarController()
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        DispatchQueue.main.async {
+            self.setupTabBar()
+            self.navigationController.setViewControllers([self.tabBarController], animated: true)
+        }
+        
+    }
+    func setupTabBar() {
+        let navigationController1 = UINavigationController(rootViewController: MainViewController())
+        let navigationController2 = UINavigationController(rootViewController: MainViewController())
+        let navigationController3 = UINavigationController(rootViewController: MainViewController())
+        let navigationController4 = UINavigationController(rootViewController: MainViewController())
+        
+        navigationController1.tabBarItem = UITabBarItem(title: "Sklad", image: UIImage(systemName: "tray.full.fill"), tag: 0)
+        navigationController2.tabBarItem = UITabBarItem(title: "List", image: UIImage(systemName: "list.bullet.circle.fill"), tag: 1)
+        navigationController3.tabBarItem = UITabBarItem(title: "Scaner", image: UIImage(systemName: "barcode"), tag: 2)
+        navigationController4.tabBarItem = UITabBarItem(title: "Account", image: UIImage(systemName: "person.crop.circle"), tag: 3)
+        
+        tabBarController.viewControllers = [navigationController1, navigationController2, navigationController3, navigationController4]
+        
+    }
+}
